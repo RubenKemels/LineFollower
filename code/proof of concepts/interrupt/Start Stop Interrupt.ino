b943db373847680c5c87a2a0b2f26daff2db183d
RubenKemels <114938034+RubenKemels@users.noreply.github.com>
2022-12-02 14:59:31 +0100

    Add files via upload

---
+++ code/proof of concepts/interrupt/Start Stop Interrupt.ino
@@ -0,0 +1,18 @@
+const int Drukknop = 3;
+const int Led = 9;
+bool toestand_Led = LOW;
+
+void setup() {
+  
+  pinMode(Drukknop, INPUT_PULLUP);
+  pinMode(Led, OUTPUT);
+  attachInterrupt(digitalPinToInterrupt(Drukknop), aan, CHANGE);
+}
+void loop() {
+   digitalWrite(Led, toestand_Led);
+}
+
+void aan() {
+  toestand_Led = !toestand_Led;
+ 
+}
